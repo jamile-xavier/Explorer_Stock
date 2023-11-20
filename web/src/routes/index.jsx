08d30868b82ef6dbf7266c4035b11e2f5a11bed7
Jamile Xavier <110774918+jamile-xavier@users.noreply.github.com>
2023-11-20 14:32:27 -0300

    Finalização da aplicação

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,16 +1,40 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 import { useAuth } from "../hooks/auth";
+import { USER_ROLE } from "../utils/roles";
 
-import { AppRoutes } from './app.routes';
-import { AuthRoutes } from './auth.routes';
+import { AdminRoutes } from "./admin.routes";
+import { CustomerRoutes } from "./customer.routes";
+import { SalesRoutes } from "./sale.routes";
+import { AuthRoutes } from "./auth.routes";
+import { useEffect } from "react";
+import { api } from "../services/api";
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  useEffect(() => {
+    api.get("/users/validated").catch((error) => {
+      if (error.response?.status === 401) {
+        signOut();
+      }
+    });
+  }, []);
+
+  function AccessRoute() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />;
+      case USER_ROLE.SALE:
+        return <SalesRoutes />;
+      default:
+        return <CustomerRoutes />;
+    }
+  }
 
   return (
-    <BrowserRouter>
-      {user ? <AppRoutes /> : <AuthRoutes />}
-    </BrowserRouter>
+    <BrowserRouter>{user ? <AccessRoute /> : <AuthRoutes />}</BrowserRouter>
   );
 }
